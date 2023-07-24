9b16b1a9d20bb5e3fe5912ec4eda097cc4110247
MatTim314 <edston47@gmail.com>
2023-07-24 17:58:19 +0200

    feat: UserForm skeleton

--- inqool_entry-task_react/src/components/UserForm.tsx
+++ inqool_entry-task_react/src/components/UserForm.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import {
+  Checkbox,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  VStack,
+  Box,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
 
 function UserForm() {
   return (
-    <div>UserForm</div>
-  )
+    <FormControl isRequired>
+      <FormLabel>Username</FormLabel>
+      <Input type="text" placeholder="Enter a username"></Input>
+
+      <Checkbox size="lg" spacing="1rem" defaultChecked>
+        List projects
+      </Checkbox>
+      <Checkbox size="lg" spacing="1rem" defaultChecked>
+        List organizations
+      </Checkbox>
+
+      <Button type="submit">Search</Button>
+    </FormControl>
+  );
 }
 
-export default UserForm
+export default UserForm;
