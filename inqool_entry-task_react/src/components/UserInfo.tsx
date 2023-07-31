6e2c6f8968a31f140622c25faf0de6433ee0bc1c
MatTim314 <edston47@gmail.com>
2023-07-31 18:03:16 +0200

    feat: username links to github

--- inqool_entry-task_react/src/components/UserInfo.tsx
+++ inqool_entry-task_react/src/components/UserInfo.tsx
@@ -1,21 +1,29 @@
 import { Avatar, Box, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 import User from '../types/User';
+import { Link } from "@chakra-ui/react";
 
 interface MyComponentProps {
   user: User;
 }
 
-function UserInfo({user} : MyComponentProps) {
+function UserInfo({ user }: MyComponentProps) {
+  
+  let bio : string = user.bio && `Bio: ${user.bio}`;
+  let href: string = user.url;
   return (
     <Box>
-      <Heading>{user.username}</Heading>
+      <Heading>
+        <Link href={href} isExternal>
+          {user.username}
+        </Link>
+      </Heading>
       <Avatar src={user.avatar_url} />
-          <Text>Bio: {user.bio} </Text>
-          <Text>Followers: {user.followers} </Text>
-          <Text>Following: {user.following} </Text>
+      <Text> {bio} </Text>
+      <Text>Followers: {user.followers} </Text>
+      <Text>Following: {user.following} </Text>
     </Box>
-    )
+  );
 }
 
 export default UserInfo
